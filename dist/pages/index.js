2916423be74040d3b7f6a3af0f8ca14f4cbc88db
kenshir0f <tmu.kenshiro@gmail.com>
2018-05-06 15:39:57 +0900

    remove dist on github

--- dist/pages/index.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import App from '../components/App';
-export default () => React.createElement(App, null,
-    React.createElement("p", null, "Index Page"));
-//# sourceMappingURL=index.js.map

cb4c0597dd3bf78423711d93c29642c7a799ac01
kenshir0f <tmu.kenshiro@gmail.com>
2018-05-06 15:52:46 +0900

    remove time delay

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,13 +6,6 @@ interface Props {
 }
 
 export default class About extends React.Component<Props> {
-  
-    static async getInitialProps({ query }) {
-      const content = await new Promise(res => setTimeout(res, 1000))
-      return {
-        message: '!!'
-      }
-    }
     
     render() {
       return (<App><p>About Page ${this.props.message}</p></App>)

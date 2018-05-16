558db75a57869a63b6d762f812ca12fb2cbf258e
kenshir0f <tmu.kenshiro@gmail.com>
2018-05-16 14:37:22 +0900

    wip

--- src/components/Molecules/Header/index.stories.tsx
+++ src/components/Molecules/Header/index.stories.tsx
@@ -4,9 +4,10 @@ import {storiesOf} from '@storybook/react'
 import {action} from '@storybook/addon-actions'
 
 import {HeaderMenuIcon} from '../../Atoms/Icon/index'
+import Img from '../../Atoms/Img/index'
 import Header from './Header'
 
 storiesOf('Header', module)
   .add('デフォルト', () => <p><HeaderMenuIcon/></p>)
   .add('デフォルト2', () => <div>hoge</div>)
-  .add('デフォルト3', () => <Header>hoge</Header>)
+  .add('デフォルト3', () => <Header></Header>)
